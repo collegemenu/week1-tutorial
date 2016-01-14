ddec2fec079f08a91b487189360b090db4a4d6c3
unknown <jmclemore3@mail.valenciacollege.edu>
2016-01-13 23:04:54 -0500

    Name Change
    Joel Mclemore

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # replace the word 'hector' with your own name
 
 def hello_world():
-	return 'hello world, my name is '
+	return 'hello world, my name is Joel Mclemore'
 
 print( hello_world() + 'hector' )
